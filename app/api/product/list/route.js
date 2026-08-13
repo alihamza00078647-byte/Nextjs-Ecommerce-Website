3c3ccd4ec6c583def27c7324844761b02efd8bad
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-08-13 14:17:16 +0500

    Backend APIs

---
+++ app/api/product/list/route.js
@@ -0,0 +1,20 @@
+import connectDB from "@/config/db";
+import authSeller from "@/lib/authSeller";
+import Product from "@/model/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request) {
+    try {
+        
+        await connectDB();
+
+        const products = await Product.find({});
+        
+        return NextResponse.json({success: true, products});
+        
+    } catch (error) {
+        return NextResponse.json({success: false, message: error.message});        
+    }
+}
--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -2,9 +2,14 @@
 import React, { useState } from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { useAppContext } from "@/context/AppContext";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const AddProduct = () => {
-
+  
+  const { getToken } = useAppContext();
+  
   const [files, setFiles] = useState([]);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
@@ -14,7 +19,41 @@ const AddProduct = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // console.log(files,name,description,category,price,offerPrice);
 
+    const formData = new FormData();
+
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('category', category);
+    formData.append('price', price);
+    formData.append('offerPrice', offerPrice);
+
+    // add images one by one
+    for (let i = 0; i < files.length; i++) {
+      formData.append('images', files[i]);
+    }
+
+    try {
+
+      const token = await getToken();
+      const {data} = await axios.post('/api/product/add', formData, {headers: {Authorization: `Bearer ${token}`}})
+      
+      if (data.success) {
+        toast.success(data.message);
+        setFiles([]);
+        setName('');
+        setDescription('');
+        setCategory('');
+        setPrice('');
+        setOfferPrice('');
+      } else {
+        toast.error(data.message);
+      }
+      
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
